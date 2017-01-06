885e3972c31f335887172b24607e660df9de5370
John Doe <wazemap@yandex.com>
2017-01-06 17:59:46 +0100

    Create plugin.js
    Fisrt try.

---
+++ plugin.js
@@ -0,0 +1,18 @@
+{
+    "moduleId" : "wazemap/tomtom",
+    "title" : "TomTom Live",
+    "subtitle" : "Traffic view from TomTom",
+    
+    "backButton" : false,
+    "enableGPS" : false,
+    "zoomControl" : false,
+    "screenLockRot" : false,
+    "reload" : true,
+    
+    "shouldOverrideUrlLoading1" : "http",
+    "shouldOverrideUrlLoading2" : "https",
+    
+    "loadUrl" : "https://mydrive.tomtom.com/en_us/#+viewport=#lat#,#lng#,#zoom#+ver=2",
+    				
+    "browserLaunchLink" : "https://mydrive.tomtom.com/en_us/#+viewport=#lat#,#lng#,#zoom#+ver=2"
+}
